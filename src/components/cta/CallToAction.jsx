2d4cc84131f3837e6a0cc65dc4ea5efb40db82a6
aslam <cntctme.aslam@gmail.com>
2021-12-18 11:30:24 +0530

    firts commit

---
+++ src/components/cta/CallToAction.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./cta"
+
+const CallToAction = () => {
+    return (
+        <div>
+           Call to action 
+        </div>
+    )
+}
+
+export default CallToAction
---
+++ src/components/favourite/Favourite.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./favourite.css"
+
+const Favourite = () => {
+    return (
+        <div>
+            Favourite
+        </div>
+    )
+}
+
+export default Favourite
